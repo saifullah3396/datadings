4b1d82726781578932ed5c5a8fbfa11deab3f675
folz <joachim.folz@dfki.de>
2017-05-04 17:15:33 +0200

    basic Writer/Reader + iSUN integration

---
+++ datadings/__init__.py
@@ -0,0 +1,88 @@
+from __future__ import print_function, division
+
+import io
+from collections import OrderedDict
+
+import numpy as np
+import msgpack
+
+
+def _default(o):
+    if isinstance(o, np.ndarray):
+        return o.tolist()
+
+
+class Writer(object):
+    def __init__(self, outfile):
+        self._path = outfile
+        self._outfile = io.FileIO(outfile, 'wb')
+        self._packer = msgpack.Packer(
+            default=_default, use_bin_type=True, encoding='utf8'
+        )
+        self._indices = OrderedDict()
+        self.written = 0
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._outfile.flush()
+        self._outfile.close()
+        try:
+            self._indices.popitem()
+        except KeyError:
+            pass
+        with io.FileIO(self._path + '.index', 'w') as f:
+            msgpack.pack(self._indices, f)
+
+    def _write(self, data):
+        self._outfile.write(self._packer.pack(data))
+        self.written += 1
+
+    def write(self, *args):
+        raise NotImplementedError()
+
+
+def _load_index(path):
+    try:
+        with io.FileIO(path, 'rb') as f:
+            return msgpack.unpack(f, encoding='utf8',
+                                  object_pairs_hook=OrderedDict)
+    except IOError:
+        return OrderedDict()
+
+
+class Reader(object):
+    def __init__(self, infile):
+        self._infile = io.FileIO(infile, 'rb')
+        self._files = _load_index(infile + '.index')
+        self._indices = self._files.values()
+        self._unpacker = msgpack.Unpacker(self._infile, encoding='utf8')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._infile.close()
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return len(self._indices)
+
+    def __next__(self):
+        return self._convert(next(self._unpacker))
+
+    next = __next__
+
+    def seek_index(self, i):
+        self._infile.seek(self._indices[i], 0)
+        self._unpacker = msgpack.Unpacker(self._infile, encoding='utf8')
+
+    def seek_file(self, name):
+        self._infile.seek(self._files[name], 0)
+        self._unpacker = msgpack.Unpacker(self._infile, encoding='utf8')
+
+    def _convert(self, item):
+        raise NotImplementedError()
---
+++ datadings/iSUN.py
@@ -0,0 +1,112 @@
+from __future__ import print_function, division
+
+import zipfile
+import os.path as pt
+from collections import namedtuple
+
+from six import text_type
+import numpy as np
+import scipy.io as sio
+
+from datadings import Writer
+from datadings import Reader
+
+
+iSUNImage = namedtuple(
+    'iSUNImage',
+    ('experiments', 'dimensions', 'filename', 'scenecategory')
+)
+iSUNExperiment = namedtuple(
+    'iSUNExperiment',
+    ('locations', 'timestamps', 'fixations')
+)
+
+
+def _convert_image(entry):
+    image = text_type(entry[0][0][0])
+    if not image.endswith('.jpg'):
+        image += '.jpg'
+    scenecategory = text_type(entry[0][2][0])
+    resolution = tuple(entry[0][1][0].tolist()[::-1])
+    try:
+        experiments = [
+            iSUNExperiment(
+                subject[0].astype(np.float32).reshape((-1, 2)),  # locations
+                subject[1].astype(np.float32).flatten(),  # timestamps
+                subject[2].astype(np.float32).reshape((-1, 2)),  # fixations
+            )
+            for whatever_this_is_thx_iSUN in entry[0][3]
+            for subject in whatever_this_is_thx_iSUN
+        ]
+    except IndexError:
+        experiments = []
+    return iSUNImage(experiments, resolution, image, scenecategory)
+
+
+def yield_isun_metadata(path):
+    data = sio.loadmat(path)
+    valid = {k: v for k, v in data.items() if not k.startswith('__')}
+    if len(valid) > 1:
+        raise ValueError('too many keys: %s' % ', '.join(valid))
+    images = list(valid.values())[0]
+    for entry in images:
+        yield _convert_image(entry)
+
+
+class ISUNWriter(Writer):
+    def write(self, jpegdata, image):
+        self._indices[image.filename] = self._outfile.tell()
+        Writer._write(self, (image, jpegdata))
+        pass
+
+
+def _write_image(image, imagezip, packer):
+    jpegdata = imagezip.read(pt.join('images', image.filename))
+    packer.write(jpegdata, image)
+    pass
+
+
+class ISUNReader(Reader):
+    def _convert(self, item):
+        jpegdata, image = item
+        image = iSUNImage(*image)
+        for i, experiment in enumerate(image.experiments):
+            image.experiments[i] = iSUNExperiment(*experiment)
+        return jpegdata, image
+
+
+def write_isun(indir, outdir):
+    import sys
+    with zipfile.ZipFile(pt.join(indir, 'image.zip')) as imagezip:
+        for name in ('training', 'validation', 'testing'):
+            print('%s...' % name, end=' ')
+            sys.stdout.flush()
+            with ISUNWriter(pt.join(outdir, name + '.msgpack')) as packer:
+                for image in yield_isun_metadata(pt.join(indir, name + '.mat')):
+                    _write_image(image, imagezip, packer)
+            print('done.')
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(
+        'indir',
+        metavar='INPATH',
+        help='directory that contains iSUN mat and zip files'
+    )
+    parser.add_argument(
+        '-o', '--outdir',
+        metavar='OUTPATH',
+        help='output directory; defaults to indir'
+    )
+    args = parser.parse_args()
+    outdir = args.outdir or args.indir
+    write_isun(args.indir, outdir)
+
+
+if __name__ == '__main__':
+    main()
