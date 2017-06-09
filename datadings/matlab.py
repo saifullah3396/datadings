ed6b9c64bc3c72113e6cd47be607e4655c7f5a08
folz <joachim.folz@dfki.de>
2017-06-09 09:07:01 +0200

    doc

--- datadings/matlab.py
+++ datadings/matlab.py
@@ -3,6 +3,11 @@ import scipy.io
 
 
 def loadmat(mat):
+    """ Load a Matlab "mat" file.
+
+        @param mat: path, file-like object, or data
+        @return: contents of the "mat" file
+    """
     try:
         return scipy.io.loadmat(mat)
     except IOError:
@@ -10,8 +15,15 @@ def loadmat(mat):
         return scipy.io.loadmat(buf)
 
 
-def iter_fields(mat, ignore=set()):
-    for k in mat.dtype.fields.keys():
+def iter_fields(arr, ignore=set()):
+    """ Iterate over the fields of a structured numpy array
+        (i.e., an array with a complex data type).
+        Each iteration yields (fieldname, value).
+
+        @param arr: a structured array
+        @param ignore: set of fields to ignore
+    """
+    for k in arr.dtype.fields.keys():
         if k in ignore:
             continue
-        yield k, mat[k]
+        yield k, arr[k]
