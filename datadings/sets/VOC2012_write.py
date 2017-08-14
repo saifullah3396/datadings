9ec112c552dff10de1c84e9c9cce7f79f948cef1
folz <joachim.folz@dfki.de>
2017-08-14 11:31:49 +0200

    VOC2012: weights for train only

--- datadings/sets/VOC2012_write.py
+++ datadings/sets/VOC2012_write.py
@@ -29,29 +29,6 @@ from datadings.sets.VOC2012 import CLASSES
 from datadings.sets.VOC2012 import M
 
 
-def calculate_weights(tar):
-    printer = FrequencyPrinter()
-    weights = defaultdict(lambda: 0)
-    for m in tar.getmembers():
-        if 'SegmentationClass' not in m.path \
-                or not m.path.endswith('.png'):
-            continue
-        segim = imagedata_to_array(extractmember(tar, m))
-        segmap = map_color_image(segim)
-        counts = np.bincount(segmap.flatten())
-        for c in np.nonzero(counts)[0]:
-            weights[c] += counts[c]
-        printer.update()
-    print_over('%d samples analyzed' % printer.total_updates)
-    total = sum(weights.values())
-    freq = {c: n/total for c, n in weights.items()}
-    median_freq = np.median(list(freq.values()))
-    # cannot serialize numpy scalars,
-    # classes & weights must be Python numbers!
-    weights = {int(c): float(median_freq/f) for c, f in freq.items()}
-    return weights
-
-
 def imagedata_to_array(data):
     bio = io.BytesIO(data)
     im = Image.open(bio).convert('RGB')
@@ -104,15 +81,14 @@ def extract(tar, path):
 
 
 def extractmember(tar, member):
-    with tar.extractfile(member) as f:
-        return f.read()
+    return tar.extractfile(member).read()
 
 
 def get_imageset(tar, path):
     return extract(tar, path).decode('utf-8').strip('\n').split('\n')
 
 
-def write_sets(indir, outdir, shuffle=True):
+def _prepare_indir(indir):
     datapath = pt.join(indir, 'VOCtrainval_11-May-2012.tar')
     download_if_not_found(
         'http://host.robots.ox.ac.uk/pascal/VOC/voc2012/'
@@ -123,8 +99,12 @@ def write_sets(indir, outdir, shuffle=True):
     sets_dir = pt.join(root_dir, 'ImageSets', 'Segmentation')
     image_dir = pt.join(root_dir, 'JPEGImages')
     seg_dir = pt.join(root_dir, 'SegmentationClass')
+    return datapath, sets_dir, image_dir, seg_dir
+
+
+def write_sets(indir, outdir, shuffle=True):
+    datapath, sets_dir, image_dir, seg_dir = _prepare_indir(indir)
     with tarfile.TarFile(datapath) as tar:
-        # weights = calculate_weights(tar)
         for split in ('train', 'val'):
             print(split)
             printer = FrequencyPrinter()
@@ -140,6 +120,38 @@ def write_sets(indir, outdir, shuffle=True):
                     write_image(writer, name, im, seg)
                     printer.update()
             printer.print_total_updates()
+
+
+def calculate_weights(indir):
+    datapath, sets_dir, _, seg_dir = _prepare_indir(indir)
+    with tarfile.TarFile(datapath) as tar:
+        for split in ('train', 'val'):
+            print(split, 'weights')
+            printer = FrequencyPrinter()
+            weights = defaultdict(lambda: 0)
+            sets_path = pt.join(sets_dir, '%s.txt' % split)
+            images = get_imageset(tar, sets_path)
+            for name in images:
+                seg = extract(tar, pt.join(seg_dir, name) + '.png')
+                segim = imagedata_to_array(seg)
+                segmap = map_color_image(segim)
+                counts = np.bincount(segmap.flatten())
+                for c in np.nonzero(counts)[0]:
+                    weights[c] += counts[c]
+                printer.update()
+            print_over('%d samples analyzed' % printer.total_updates)
+            total = sum(weights.values())
+            freq = {c: n / total for c, n in weights.items()}
+            median_freq = np.median(list(freq.values()))
+            # cannot serialize numpy scalars,
+            # classes & weights must be Python numbers!
+            weights = {int(c): float(median_freq / f)
+                       for c, f in freq.items()}
+            print('WEIGHTS = [')
+            for c in sorted(weights):
+                print('    {},'.format(weights[c]))
+            print(']')
+            print()
 
 
 def main():
@@ -159,9 +171,17 @@ def main():
         metavar='OUTPATH',
         help='output directory; defaults to indir'
     )
+    parser.add_argument(
+        '--calculate-weights',
+        action='store_true',
+        help='calculate median-frequency class weights'
+    )
     args = parser.parse_args()
     outdir = args.outdir or args.indir
-    write_sets(args.indir, outdir)
+    if args.calculate_weights:
+        calculate_weights(args.indir)
+    else:
+        write_sets(args.indir, outdir)
 
 
 if __name__ == '__main__':
