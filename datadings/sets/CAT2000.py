b9ef2816fbb80cff0117a39135be8d71e21ccd27
Philipp Liznerski <p_liznersk13@cs.uni-kl.de>
2017-10-18 18:28:08 +0200

    CAT2000 Reader now uses fixations if available

--- datadings/sets/CAT2000.py
+++ datadings/sets/CAT2000.py
@@ -1,2 +1,15 @@
 from datadings.sets import convert_saliency as convert_cat2000
-from datadings.sets import SaliencyReader as CAT2000Reader
+from datadings.sets import SaliencyData, SaliencyExperiment
+from datadings.reader import MsgpackReader
+
+def convert_saliency(item):
+    return SaliencyData(
+            item[0],
+            [SaliencyExperiment(*experiment[:2] if len(experiment) < 4 # locations
+                                else [experiment[3], experiment[1]]) for experiment in item[1]], # fixations
+            item[2],
+    )
+
+
+class CAT2000Reader(MsgpackReader):
+    _convert = staticmethod(convert_saliency)
