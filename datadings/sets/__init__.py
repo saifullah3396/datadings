220060fbdf23f841c0b68419b3e06bdfc3e2735b
folz <joachim.folz@dfki.de>
2018-01-31 11:55:06 +0100

    datatypes are functions now to create unicode keys

--- datadings/sets/__init__.py
+++ datadings/sets/__init__.py
@@ -3,39 +3,37 @@ from __future__ import unicode_literals
 
 def datatype(name, *keys):
     args = ', '.join(keys)
-    kwargs = ', '.join('{0[0]}={0[0]}'.format((v,)) for v in keys)
-    code = 'def __init__(self, {args}): dict.__init__(self, {kwargs})'\
-        .format(name=name, args=args, kwargs=kwargs)
-    ns = {}
-    exec(code, {}, ns)
-    return type(name, (dict,), ns)
+    values = ', '.join('u"{0[0]}":{0[0]}'.format((v,)) for v in keys)
+    code = 'def {name}({args}): return {{ {values} }}'\
+        .format(name=name.decode('ascii'), args=args, values=values)
+    exec(code, {}, globals())
 
 
-ImageClassificationData = datatype(
+datatype(
     b'ImageClassificationData',
     'image', 'label', 'key',
 )
-ImageSegmentationData = datatype(
+datatype(
     b'ImageSegmentationData',
     'image', 'target_image', 'key', 'classes', 'class_weights',
 )
-MaskedImageSegmentationData = datatype(
+datatype(
     b'MaskedImageSegmentationData',
     'image', 'label_image', 'mask', 'key', 'classes', 'class_weights',
 )
-SegmentationDisparityData = datatype(
+datatype(
     b'SegmentationDisparityData',
     'image', 'disparity_map', 'label_image', 'key', 'classes', 'class_weights',
 )
-SaliencyData = datatype(
+datatype(
     b'SaliencyData',
     'image', 'experiments', 'key',
 )
-SaliencyExperiment = datatype(
+datatype(
     b'SaliencyExperiment',
     'locations', 'map',
 )
-UnsupervisedImageData = datatype(
+datatype(
     b'UnsupervisedImageData',
     'image', 'key',
 )
