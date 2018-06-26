790588d6b6cdb8c0cd57ee64884395811111cdae
folz <joachim.folz@dfki.de>
2018-06-26 14:42:51 +0200

    simple tool to cat msgpack files

--- setup.py
+++ setup.py
@@ -69,7 +69,8 @@ scripts = [
     'bin/datadings-show',
     'bin/datadings-shuffle',
     'bin/datadings-write',
-    'bin/datadings-split'
+    'bin/datadings-split',
+    'bin/datadings-cat'
 ]
 
 
