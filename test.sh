5a479da7242cb5765fa1572b12def577c3997025
Unknown <j_bloos13@cs.uni-kl.de>
2019-07-11 11:55:37 +0200

    didn't work. Try it with different python call

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@
 /opt/python/cp36-cp36m/bin/pip install pytest
 /opt/python/cp36-cp36m/bin/pip install pytest-cov
 cd test
-LIBDIR=`python -c"import os.path as pt; import datadings; print(pt.dirname(datadings.__file__))"`
-pytest --cov=$LIBDIR . --cov-report term-missing
+LIBDIR=`/opt/python/cp36-cp36m/bin/python -c"import os.path as pt; import datadings; print(pt.dirname(datadings.__file__))"`
+/opt/python/cp36-cp36m/bin/python -m pytest --cov=$LIBDIR . --cov-report term-missing
