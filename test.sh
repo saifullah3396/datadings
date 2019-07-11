719de81484a8fa989d8f008a33f35b0c879e126f
Unknown <j_bloos13@cs.uni-kl.de>
2019-07-11 13:09:19 +0200

    fix images in CI

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ echo "$(/opt/python/${PYVER}/bin/python --version)"
 /opt/python/${PYVER}/bin/pip install pytest-cov
 cd test
 
-LIBDIR=`/opt/python/*${PYVER}*/bin/python -c"import os.path as pt; import datadings; print(pt.dirname(datadings.__file__))"`
+LIBDIR=`/opt/python/${PYVER}/bin/python -c"import os.path as pt; import datadings; print(pt.dirname(datadings.__file__))"`
 /opt/python/${PYVER}/bin/python -m pytest --cov=$LIBDIR * --cov-report term-missing
