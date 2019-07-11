d2fd5f963a404e253e0579315ea813740683b41d
Unknown <j_bloos13@cs.uni-kl.de>
2019-07-11 11:52:20 +0200

    fix test.sh (datadings instead of crumpets)
    copy paste mistake

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@
 /opt/python/cp36-cp36m/bin/pip install pytest
 /opt/python/cp36-cp36m/bin/pip install pytest-cov
 cd test
-LIBDIR=`python -c"import os.path as pt; import crumpets; print(pt.dirname(crumpets.__file__))"`
+LIBDIR=`python -c"import os.path as pt; import datadings; print(pt.dirname(datadings.__file__))"`
 pytest --cov=$LIBDIR . --cov-report term-missing
