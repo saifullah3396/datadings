2c23798f8b78dd9e5ef35c2792e41be72398b1c1
Unknown <j_bloos13@cs.uni-kl.de>
2019-07-11 13:02:19 +0200

    remove * signs in python path

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "running tests for python version:"
-echo "$(/opt/python/*${PYVER}*/bin/python --version)"
-/opt/python/*${PYVER}*/bin/pip install *.whl
-/opt/python/*${PYVER}*/bin/pip install pytest
-/opt/python/*${PYVER}*/bin/pip install pytest-cov
+echo "$(/opt/python/${PYVER}/bin/python --version)"
+/opt/python/${PYVER}/bin/pip install *.whl
+/opt/python/${PYVER}/bin/pip install pytest
+/opt/python/${PYVER}/bin/pip install pytest-cov
 cd test
 
 LIBDIR=`/opt/python/*${PYVER}*/bin/python -c"import os.path as pt; import datadings; print(pt.dirname(datadings.__file__))"`
-/opt/python/*${PYVER}*/bin/python -m pytest --cov=$LIBDIR * --cov-report term-missing
+/opt/python/${PYVER}/bin/python -m pytest --cov=$LIBDIR * --cov-report term-missing
