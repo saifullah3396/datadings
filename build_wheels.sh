69001d6bfa9e2378101c453b2bde8be5b12e580a
Unknown <j_bloos13@cs.uni-kl.de>
2019-07-11 13:42:54 +0200

    now it really builds with different python versions

--- build_wheels.sh
+++ build_wheels.sh
@@ -3,10 +3,10 @@ set -e -x
 
 # Install native dependencies
 # yum install ...
-/opt/python/cp36-cp36m/bin/pip install six
+/opt/python/${PYVER}/bin/pip install six
 
 # Compile wheels
-/opt/python/cp36-cp36m/bin/python setup.py bdist_wheel --dist-dir=.
+/opt/python/${PYVER}/bin/python setup.py bdist_wheel --dist-dir=.
 rm -rf build dist datadings.egg-info
 
 # Turn normal wheels into manylinux wheels:
