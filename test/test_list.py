475e268ab4a800a198e28ec74a8d8ade98a9faa7
Joachim Folz <j_folz@cs.uni-kl.de>
2020-05-14 18:55:48 +0200

    remove numpy from test

--- test/test_list.py
+++ test/test_list.py
@@ -12,17 +12,27 @@ for p in (os.pardir, parent):
         pass
 print(sys.path)
 
-
-import numpy as np
 import random
+
 from datadings.reader import ListReader
+
+
+def floatrange(start, stop=None, step=None):
+    a = 0 if stop is None else start
+    b = start if stop is None else stop
+    step = step or 1
+    print(a, b, step)
+    delta = b - a
+    steps = int(delta / step)
+    for x in range(steps):
+        yield delta * x/steps + a
 
 
 def test_checkAllKeys():
     count = 0
     samples = []
     map = {}
-    for data in np.arange(10,20,0.1):
+    for data in floatrange(10, 20, 0.1):
         sample = {"data": data, "key": count, "label": random.random()}
         samples.append(sample)
         map[count] = sample
@@ -38,7 +48,7 @@ def test_checkAllKeys():
 def test_checkEndOfIteration():
     count = 0
     samples = []
-    for data in np.arange(10,20,0.1):
+    for data in floatrange(10, 20, 0.1):
         sample = {"data": data, "key": count, "label": random.random()}
         samples.append(sample)
         count = count +1
@@ -51,10 +61,11 @@ def test_checkEndOfIteration():
             print("this should never be reached")
             assert False
 
+
 def test_multipleIterations():
     count = 0
     samples = []
-    for data in np.arange(10,20,0.1):
+    for data in floatrange(10, 20, 0.1):
         sample = {"data": data, "key": count, "label": random.random()}
         samples.append(sample)
         count = count +1
@@ -73,11 +84,12 @@ def test_multipleIterations():
             print (test)
     assert reached1 and reached2
 
+
 def test_seek_keys():
     count = 0
     samples = []
     map = {}
-    for data in np.arange(10, 20, 0.1):
+    for data in floatrange(10, 20, 0.1):
         sample = {"data": data, "key": count, "label": random.random()}
         samples.append(sample)
         map[count] = sample
@@ -90,11 +102,12 @@ def test_seek_keys():
             map.pop(test["key"])
     assert len(map) == 10 #because the reader is ordered
 
+
 def test_seek_index():
     count = 0
     samples = []
     map = {}
-    for data in np.arange(10, 20, 0.1):
+    for data in floatrange(10, 20, 0.1):
         sample = {"data": data, "key": count, "label": random.random()}
         samples.append(sample)
         map[count] = sample
@@ -106,4 +119,3 @@ def test_seek_index():
         for test in reader:
             map.pop(test["key"])
     assert len(map) == 10
-
