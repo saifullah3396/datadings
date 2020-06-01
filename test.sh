ea1a6231f4108e0db39e0ee4993383e06cf56a59
folz <joachim.folz@dfki.de>
2020-06-01 19:59:12 +0200

    ../?

--- test.sh
+++ test.sh
@@ -2,15 +2,15 @@
 set -e -x
 
 for PYBIN in /opt/python/*/bin; do
-  "${PYBIN}/pip" install -r requirements.txt
-  "${PYBIN}/pip" install -r test-requirements.txt
-  "${PYBIN}/pip" install datadings --no-index -f dist --no-deps
-  LIBDIR=$(
-    "${PYBIN}/python" -c \
-    "import os.path as pt; import datadings; print(pt.dirname(datadings.__file__))"
-  )
   (
     cd test
+    "${PYBIN}/pip" install -r ../requirements.txt
+    "${PYBIN}/pip" install -r ../test-requirements.txt
+    "${PYBIN}/pip" install datadings --no-index -f ../dist --no-deps
+    LIBDIR=$(
+      "${PYBIN}/python" -c \
+      "import os.path as pt; import datadings; print(pt.dirname(datadings.__file__))"
+    )
     "${PYBIN}/python" -m pytest -vv
     "${PYBIN}/python" -m pytest --cov="${LIBDIR}" ./* --cov-report term-missing
   )
