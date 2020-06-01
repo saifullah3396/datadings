b5cc40cadb5ba403786e15b5448acb0900887e86
folz <joachim.folz@dfki.de>
2020-06-01 18:44:15 +0200

    fix file paths

--- test.sh
+++ test.sh
@@ -2,15 +2,15 @@
 set -e -x
 
 for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install -r ../test-requirements.txt
-    "${PYBIN}/pip" install datadings --no-index -f ../dist
-    LIBDIR=$(
-        "${PYDIR}"/bin/python -c \
-        "import os.path as pt; import datadings; print(pt.dirname(datadings.__file__))"
-    )
-    (
+  "${PYBIN}/pip" install -r test-requirements.txt
+  "${PYBIN}/pip" install datadings --no-index -f dist
+  LIBDIR=$(
+    "${PYBIN}/python" -c \
+    "import os.path as pt; import datadings; print(pt.dirname(datadings.__file__))"
+  )
+  (
     cd test
     "${PYBIN}/python" -m pytest -vv
-    "${PYDIR}"/bin/python -m pytest --cov="${LIBDIR}" ./* --cov-report term-missing
-    )
+    "${PYBIN}/python" -m pytest --cov="${LIBDIR}" ./* --cov-report term-missing
+  )
 done
