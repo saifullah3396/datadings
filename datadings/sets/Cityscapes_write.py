4dff51f8a50c700afd86ae0b99a0ee3160831d57
folz <joachim.folz@dfki.de>
2020-06-03 11:46:04 +0200

    Cityscapes: set total for writer

--- datadings/sets/Cityscapes_write.py
+++ datadings/sets/Cityscapes_write.py
@@ -24,22 +24,24 @@ DISPARITY = 'disparity_trainvaltest.zip'
 GT = 'gtFine_trainvaltest.zip'
 
 
-def yield_samples(split, leftzip, disparityzip, gtzip):
-    for left_name in leftzip.namelist():
-        if not left_name.endswith('_leftImg8bit.png'):
-            continue
-        key = left_name.replace('_leftImg8bit.png', '').replace('leftImg8bit/', '')
-        if not key.startswith(split):
-            continue
-        image = leftzip.read(left_name)
+def get_keys(leftzip, split):
+    keys = [name.replace('_leftImg8bit.png', '').replace('leftImg8bit/', '')
+            for name in leftzip.namelist()
+            if name.endswith('_leftImg8bit.png')]
+    return [key for key in keys if key.startswith(split)]
+
+
+def yield_samples(keys, leftzip, disparityzip, gtzip):
+    for key in keys:
+        image = leftzip.read('leftImg8bit/' + key + '_leftImg8bit.png')
         label_image = gtzip.read('gtFine/' + key + '_gtFine_labelIds.png')
         disparity_image = disparityzip.read('disparity/' + key + '_disparity.png')
         yield key, image, label_image, disparity_image
 
 
-def write_set(outdir, split, gen):
+def write_set(outdir, split, gen, total):
     outpath = pt.join(outdir, split + '.msgpack')
-    with FileWriter(outpath) as writer:
+    with FileWriter(outpath, total=total) as writer:
         for sample in gen:
             writer.write(SegmentationDisparityData(*sample))
 
@@ -49,8 +51,9 @@ def write_sets(indir, outdir):
         return zipfile.ZipFile(pt.join(indir, path))
     with z(LEFT) as left, z(DISPARITY) as disparity, z(GT) as gt:
         for split in ('test', 'val', 'train'):
-            gen = yield_threaded(yield_samples(split, left, disparity, gt))
-            write_set(outdir, split, gen)
+            keys = get_keys(left, split)
+            gen = yield_threaded(yield_samples(keys, left, disparity, gt))
+            write_set(outdir, split, gen, len(keys))
 
 
 def main():
