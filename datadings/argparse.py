5750d162fc093e8be4351f4b46a2897da49ac02b
folz <joachim.folz@dfki.de>
2020-06-09 12:44:55 +0200

    argparse pep8

--- datadings/argparse.py
+++ datadings/argparse.py
@@ -117,6 +117,7 @@ argument_skip_verification = __make_argument(
     help='Skip verification of source files.'
 )
 
+
 argument_calculate_weights = __make_argument(
     '--calculate-weights',
     action='store_true',
