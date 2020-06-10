e15b6decbfa82ef98097577825c326d797bd67bb
folz <joachim.folz@dfki.de>
2020-06-10 22:03:52 +0200

    cat: prevent BrokenPipeError on exit

--- datadings/commands/cat.py
+++ datadings/commands/cat.py
@@ -1,5 +1,7 @@
 """Cat msgpack files.
 """
+import os
+import sys
 import os.path as pt
 import reprlib
 
@@ -9,6 +11,7 @@ from ..reader import MsgpackReader
 def cat(infile, maxstring):
     r = reprlib.Repr()
     r.maxstring = maxstring
+    r.maxdict = 2**32-1
     reader = MsgpackReader(infile)
     for sample in reader:
         print(r.repr(sample))
@@ -33,11 +36,17 @@ def main():
 
 def entry():
     try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print()
+        try:
+            main()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            print()
+    except BrokenPipeError:
+        # Python flushes standard streams on exit; redirect remaining output
+        # to devnull to avoid another BrokenPipeError at shutdown
+        devnull = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(devnull, sys.stdout.fileno())
 
 
 if __name__ == '__main__':
