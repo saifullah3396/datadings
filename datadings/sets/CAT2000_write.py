ba37c56a3e1a62a29661ba8dfe912554831556a4
folz <joachim.folz@dfki.de>
2020-06-12 11:32:08 +0200

    modernize CAT2000

--- datadings/sets/CAT2000_write.py
+++ datadings/sets/CAT2000_write.py
@@ -5,24 +5,41 @@ The data set is described here:
 
 This tool will look for the following files in the input directory
 and download them if necessary:
-    - ALLSTIMULI.zip
-    - DATA.zip
+    - trainSet.zip
+    - testSet.zip
 """
-import io
 import os
 import os.path as pt
 import zipfile
 import random
+from multiprocessing.dummy import Pool as ThreadPool
 
 import numpy as np
 from PIL import Image
 from PIL import ImageChops
+from simplejpeg import decode_jpeg
+from simplejpeg import encode_jpeg
 
 from ..writer import FileWriter
-from ..tools import download_if_not_found
 from ..matlab import loadmat
+from ..tools import yield_threaded
 from . import SaliencyData
 from . import SaliencyExperiment
+
+
+BASE_URL = 'http://saliency.mit.edu/'
+FILES = {
+    'train': {
+        'path': 'trainSet.zip',
+        'url': BASE_URL+'trainSet.zip',
+        'md5': '56ad5c77e6c8f72ed9ef2901628d6e48',
+    },
+    'test': {
+        'path': 'testSet.zip',
+        'url': BASE_URL+'testSet.zip',
+        'md5': '903ec668df2e5a8470aef9d8654e7985',
+    }
+}
 
 
 def __find_bbox(im):
@@ -43,10 +60,14 @@ def __transform_image(im, bbox, size=1024):
     )
 
 
-def __crompress(image, format='jpeg', quality=90):
-    buf = io.BytesIO()
-    image.save(buf, format=format, quality=quality)
-    return buf.getvalue()
+def __decode(data):
+    return Image.fromarray(decode_jpeg(
+        data, fastupsample=False, fastdct=False
+    ), 'RGB')
+
+
+def __crompress(image, quality=90):
+    return encode_jpeg(np.array(image), quality=quality)
 
 
 def __load_fixmap(imagezip, stimuluspath):
@@ -74,56 +95,76 @@ def filter_invalid_fixpoints(points, size):
     return points[ind]
 
 
-def write_image(imagezip, stimuluspath, writer):
-    with imagezip.open(stimuluspath) as f:
-        stimulus = Image.open(f)
-        bbox = __find_bbox(stimulus)
-        r, cropped = __transform_image(stimulus, bbox)
-        stimulusdata = __crompress(cropped)
-    try:
-        response = __load_fixmap(imagezip, stimuluspath)
+def yield_samples(imagezip, names):
+    for stimuluspath in names:
+        with imagezip.open(stimuluspath) as f:
+            stimulusdata = f.read()
+            try:
+                response = __load_fixmap(imagezip, stimuluspath)
+            except KeyError:
+                response = None
+        yield stimuluspath, stimulusdata, response
+
+
+def create_sample(item):
+    stimuluspath, stimulusdata, response = item
+    stimulus = __decode(stimulusdata)
+    bbox = __find_bbox(stimulus)
+    r, cropped = __transform_image(stimulus, bbox)
+    stimulusdata = __crompress(cropped)
+    if response is not None:
         locations = transform_points(find_fixpoints(response), bbox, r)
         locations = filter_invalid_fixpoints(locations, cropped.size)
-    except KeyError:
+    else:
         locations = None
     filename = os.sep.join(stimuluspath.split(os.sep)[-2:])
-    item = SaliencyData(
+    return SaliencyData(
         filename,
         stimulusdata,
         [SaliencyExperiment(locations, None)],
     )
-    writer.write(item)
 
 
 def __is_stimulus(path):
     return 'Stimuli' in path and 'Output' not in path and path.endswith('.jpg')
 
 
-def write_sets(indir, outdir, shuffle=True):
-    url_prefix = 'http://saliency.mit.edu/'
-    for name in ('train', 'test'):
-        imagepath = pt.join(indir, name + 'Set.zip')
-        download_if_not_found(url_prefix + '%sSet.zip' % name, imagepath)
-        with zipfile.ZipFile(imagepath) as imagezip:
-            names = [f for f in imagezip.namelist() if __is_stimulus(f)]
-            if shuffle:
-                random.shuffle(names)
-            with FileWriter(pt.join(outdir, name + '.msgpack'), total=len(names)) as writer:
-                for path in names:
-                    write_image(imagezip, path, writer)
+def write_set(imagezip, outdir, split, args):
+    names = [f for f in imagezip.namelist() if __is_stimulus(f)]
+    if args.shuffle:
+        random.shuffle(names)
+
+    gen = yield_threaded(yield_samples(imagezip, names))
+
+    outfile = pt.join(outdir, split + '.msgpack')
+    with FileWriter(outfile, total=len(names), overwrite=args.no_confirm) as writer:
+        pool = ThreadPool(args.threads)
+        for sample in pool.imap_unordered(create_sample, gen):
+            writer.write(sample)
+
+
+def write_sets(files, outdir, args):
+    for split in ('train', 'test'):
+        with zipfile.ZipFile(files[split]['path']) as imagezip:
+            try:
+                write_set(imagezip, outdir, split, args)
+            except FileExistsError:
+                pass
 
 
 def main():
-    from datadings.argparse import make_parser
-    from datadings.argparse import argument_indir
-    from datadings.argparse import argument_outdir
+    from ..argparse import make_parser
+    from ..argparse import argument_threads
+    from ..tools import prepare_indir
 
     parser = make_parser(__doc__)
-    argument_indir(parser)
-    argument_outdir(parser)
+    argument_threads(parser)
     args = parser.parse_args()
     outdir = args.outdir or args.indir
-    write_sets(args.indir, outdir)
+
+    files = prepare_indir(FILES, args)
+
+    write_sets(files, outdir, args)
 
 
 if __name__ == '__main__':
