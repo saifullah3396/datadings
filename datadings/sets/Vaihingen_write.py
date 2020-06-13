86c34ce08d132e750ef7cc7e975dabb08df06ae3
folz <joachim.folz@dfki.de>
2020-06-13 18:04:39 +0200

    modernize Vaihingen

--- datadings/sets/Vaihingen_write.py
+++ datadings/sets/Vaihingen_write.py
@@ -1,11 +1,13 @@
 """Create Vaihingen data set files.
 
 The data set is described here:
-    xxx
+    http://www2.isprs.org/commissions/comm3/wg4/detection-and-reconstruction.html
 
-This tool will look for the following files in the input directory
-and download them if necessary:
-    - Vaihingen Dataset
+Requests to download the dataset can be made by filling out the a form on
+the website.
+
+This tool will look for the ISPRS_semantic_labeling_Vaihingen directory in the
+input directory.
 
 """
 import numpy as np
@@ -61,56 +63,47 @@ def images_label_dsm_iter(indir, image_ids):
         yield fn, train_img, train_labels, train_dsm
 
 
-def write_sets(indir, outdir, crop_size=(CROP_SIZE, CROP_SIZE)):
+def write_sets(indir, outdir, args, crop_size=(CROP_SIZE, CROP_SIZE)):
     # Training-Split -> give whole image
     train_file = pt.join(outdir, 'Vaihingen_train.msgpack')
-    with FileWriter(train_file) as writer:
-        image_ids = [1, 3, 5, 7, 13, 17, 21, 23, 26, 32, 37]
-        for fn, train_img, train_labels, train_dsm in \
-                images_label_dsm_iter(indir, image_ids):
-
-            write(writer, train_img, train_labels, train_dsm, fn)
+    image_ids = [1, 3, 5, 7, 13, 17, 21, 23, 26, 32, 37]
+    try:
+        with FileWriter(train_file, overwrite=args.no_confirm) as writer:
+            for fn, train_img, train_labels, train_dsm in \
+                    images_label_dsm_iter(indir, image_ids):
+                write(writer, train_img, train_labels, train_dsm, fn)
+    except FileExistsError:
+        pass
 
     # Validation-Split -> give splitted images
     val_file = pt.join(outdir, 'Vaihingen_val.msgpack')
-    with FileWriter(val_file) as writer:
-        for fn, train_img, train_labels, train_dsm in \
-                images_label_dsm_iter(indir, [11, 15, 28, 30, 34]):
-
-            train_labels = np.expand_dims(train_labels, axis=0)
-            train_dsm = np.expand_dims(train_dsm, axis=0)
-
-            for idx, (sub_img, sub_label, sub_dsm) in \
-                    enumerate(zip(split_array(train_img, *crop_size),
-                                  split_array(train_labels, *crop_size),
-                                  split_array(train_dsm, *crop_size))):
-                sub_img = np.array(sub_img).astype(np.uint8)
-                sub_label = np.array(sub_label[0]).astype(np.int64)
-                sub_dsm = np.array(sub_dsm[0])
-                write(writer, sub_img, sub_label, sub_dsm, "%s_%s" % (fn, idx))
+    try:
+        with FileWriter(val_file) as writer:
+            for fn, train_img, train_labels, train_dsm in \
+                    images_label_dsm_iter(indir, [11, 15, 28, 30, 34]):
+                train_labels = np.expand_dims(train_labels, axis=0)
+                train_dsm = np.expand_dims(train_dsm, axis=0)
+                gen = enumerate(zip(
+                    split_array(train_img, *crop_size),
+                    split_array(train_labels, *crop_size),
+                    split_array(train_dsm, *crop_size),
+                ))
+                for idx, (sub_img, sub_label, sub_dsm) in gen:
+                    sub_img = np.array(sub_img).astype(np.uint8)
+                    sub_label = np.array(sub_label[0]).astype(np.int64)
+                    sub_dsm = np.array(sub_dsm[0])
+                    write(writer, sub_img, sub_label, sub_dsm, "%s_%s" % (fn, idx))
+    except FileExistsError:
+        pass
 
 
 def main():
-    import argparse
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-    parser.add_argument(
-        'indir',
-        metavar='INPATH',
-        default='.',
-        help='directory that contains Vaihingen Dataset files'
-    )
-    parser.add_argument(
-        '-o', '--outdir',
-        metavar='OUTPATH',
-        help='output directory; defaults to indir'
-    )
+    from ..argparse import make_parser
+
+    parser = make_parser(__doc__, skip_verification=False, shuffle=False)
     args = parser.parse_args()
     outdir = args.outdir or args.indir
-    print(args.indir)
-    write_sets(args.indir, outdir)
+    write_sets(args.indir, outdir, args)
 
 
 if __name__ == '__main__':
