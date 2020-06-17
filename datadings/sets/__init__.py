7cb8af558dcd48c3de66fb03a68e5828cf409ac8
folz <joachim.folz@dfki.de>
2020-06-17 16:03:53 +0200

    _types.py -> types.py so it shows up in the documentation

--- datadings/sets/__init__.py
+++ datadings/sets/__init__.py
@@ -1,1 +1,5 @@
-from ._types import *
+"""
+This package contains modules to create dataset files for all supported
+datasets, as well as metadata required to work with them.
+"""
+from .types import *
