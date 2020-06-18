c6df98a7c91da5547a78e49cf3457ba23bd081bd
folz <joachim.folz@dfki.de>
2020-06-18 09:44:28 +0200

    commands init: list of available commands

--- datadings/commands/__init__.py
+++ datadings/commands/__init__.py
@@ -5,4 +5,23 @@ where ``*`` is replaced with one of the submodule names,
 The main tool to interact with datadings in this way is
 :py:mod:`datadings-write <datadings.commands.write>`.
 It finds available datasets and runs their writing scripts.
+
+These are the available tools:
+
+    - :py:mod:`datadings-write <datadings.commands.write>`
+      creates new dataset files.
+    - :py:mod:`datadings-cat <datadings.commands.cat>`
+      prints the (abbreviated) contents of a dataset file.
+    - :py:mod:`datadings-shuffle <datadings.commands.shuffle>`
+      shuffles an existing dataset file.
+    - :py:mod:`datadings-merge <datadings.commands.merge>`
+      merges two or more dataset files.
+    - :py:mod:`datadings-split <datadings.commands.split>`
+      splits a dataset file into two or more subsets.
+    - :py:mod:`datadings-bench <datadings.commands.bench>`
+      runs some basic read performance benchmarks.
+
+You can either call them directly or run them as modules
+with ``python -m datadings.commands.*``, again with star
+replaced by the name the command, e.g., ``write``.
 """
