4b73a4c029da00a4ebfd3ad11068b12333e8ee13
folz <joachim.folz@dfki.de>
2020-06-18 09:44:07 +0200

    merge: fix list formatting

--- datadings/commands/merge.py
+++ datadings/commands/merge.py
@@ -4,7 +4,7 @@ Available strategies:
 
     - concat: Concat input files in the order they are given.
     - random: Choose input file to read next sample from randomly, with
-              probability depending on the relative size of datasets.
+      probability depending on the relative size of datasets.
 """
 import sys
 import os.path as pt
