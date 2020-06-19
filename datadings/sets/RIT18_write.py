138939b093029652182fd5815f2e606e07683838
folz <joachim.folz@dfki.de>
2020-06-19 10:01:29 +0200

    refactor RIT18 + warnings about arrays instead of images

--- datadings/sets/RIT18_write.py
+++ datadings/sets/RIT18_write.py
@@ -8,6 +8,9 @@ and download them if necessary:
 See also:
     https://github.com/rmkemker/RIT-18
 
+Important:
+    Images, labels, and masks are numpy arrays, not images!
+
 """
 import os.path as pt
 import numpy as np
@@ -41,37 +44,50 @@ def write(writer, img, labels, mask, filename):
     ))
 
 
-def write_sets(files, outdir, args, crop_size=(CROP_SIZE, CROP_SIZE)):
-    dataset = loadmat(files['mat']['path'])
-
+def write_train(outdir, dataset, args):
     # Training-Split -> give whole image
     train_labels = dataset['train_labels'].astype(np.int64)
     train_data = dataset['train_data']
     train_mask = train_data[-1].astype(np.uint8)
     train_img = train_data[:6].astype(np.uint16)
-
     outfile = pt.join(outdir, 'train.msgpack')
     with FileWriter(outfile, total=1, overwrite=args.no_confirm) as writer:
         write(writer, train_img, train_labels, train_mask, "train")
 
+
+def write_set(outdir, split, labels, data, args, crop_size):
     # Validation & Test-Split -> give splitted images
+    labels = np.expand_dims(labels, axis=0)
+    gen = enumerate(zip(split_array(data, *crop_size),
+                        split_array(labels, *crop_size)))
+
+    outfile = pt.join(outdir, split + '.msgpack')
+    with FileWriter(outfile, overwrite=args.no_confirm) as writer:
+        for idx, (sub_data, sub_label) in gen:
+            sub_label = np.array(sub_label[0]).astype(np.int64)  # squeeze again!
+            sub_mask = np.array(sub_data[-1]).astype(np.uint8)
+            sub_img = np.array(sub_data[:6]).astype(np.uint16)
+            write(writer, sub_img, sub_label, sub_mask, "%s_%s" % (split, idx))
+
+
+def write_sets(files, outdir, args, crop_size=(CROP_SIZE, CROP_SIZE)):
+    dataset = loadmat(files['mat']['path'])
+
+    try:
+        write_train(outdir, dataset, args)
+    except FileExistsError:
+        pass
+
     for split in ("val",):
         data = dataset['%s_data' % (split,)]
         if split == "val":
             labels = dataset['val_labels']
         else:
             labels = np.zeros(np.array(data).shape)
-        labels = np.expand_dims(labels, axis=0)
-        gen = enumerate(zip(split_array(data, *crop_size),
-                            split_array(labels, *crop_size)))
-
-        outfile = pt.join(outdir, split+'.msgpack')
-        with FileWriter(outfile, overwrite=args.no_confirm) as writer:
-            for idx, (sub_data, sub_label) in gen:
-                sub_label = np.array(sub_label[0]).astype(np.int64)  # squeeze again!
-                sub_mask = np.array(sub_data[-1]).astype(np.uint8)
-                sub_img = np.array(sub_data[:6]).astype(np.uint16)
-                write(writer, sub_img, sub_label, sub_mask, "%s_%s" % (split, idx))
+        try:
+            write_set(outdir, split, labels, data, args, crop_size)
+        except FileExistsError:
+            pass
 
 
 def main():
