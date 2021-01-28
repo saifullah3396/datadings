b451b85594483824c267a5803c600239d0ea1d7a
folz <joachim.folz@dfki.de>
2021-01-28 11:34:06 +0100

    don't test python 3.5

--- build_wheels.sh
+++ build_wheels.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e -x
 
-# Python 2.x and 3.4 are not supported
+# Python 2.x, 3.4, 3.5 are not supported
 # remove binaries if still present
 rm -rf /opt/python/cp2*
 rm -rf /opt/python/cp34*
+rm -rf /opt/python/cp35*
 
 # make wheel with Python 3.8
 /opt/python/cp38-cp38/bin/python setup.py bdist_wheel
