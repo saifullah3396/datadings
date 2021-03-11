e780442b9f898beb8be12da7ddb0f23c3027ed0e
folz <joachim.folz@dfki.de>
2021-03-11 16:32:15 +0100

    ShardedReader: open reader on demand
    slow but should prevent OSError: [Errno 24] Too many open files

--- datadings/reader/sharded.py
+++ datadings/reader/sharded.py
@@ -1,43 +1,58 @@
 import itertools as it
 from bisect import bisect_left
+from glob import iglob
 
 from .reader import Reader
+from .msgpack import MsgpackReader
 
 
 class ShardedReader(Reader):
-    def __init__(self, readers):
+    def __init__(self, paths):
         super().__init__()
-        self._readers = readers
+        if isinstance(paths, str):
+            if '*' in paths:
+                paths = iglob(paths)
+            else:
+                raise ValueError('need multiple paths or glob pattern')
+        self._paths = paths
+        self._num_shards = len(paths)
         self._offsets = list(it.accumulate(it.chain(
-            (0,), (len(reader) for reader in readers)
+            (0,), (self._reader(i) for i in range(self._num_shards))
         )))
         self._len = self._offsets.pop(-1)
+
+    def _reader(self, index):
+        return MsgpackReader(self._paths[index])
 
     def __len__(self):
         return self._len
 
     def __contains__(self, key):
-        return any(key in reader for reader in self._readers)
+        return any(key in self._reader(i) for i in range(self._num_shards))
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        for reader in self._readers:
-            reader.__exit__(exc_type, exc_val, exc_tb)
+        # for reader in self._readers:
+        #     reader.__exit__(exc_type, exc_val, exc_tb)
+        pass
 
     def find_key(self, index):
         reader_i = bisect_left(self._offsets, index)
         offset = index - self._offsets[reader_i]
-        return self._readers[reader_i].find_key(offset)
+        with self._reader(reader_i) as reader:
+            return reader.find_key(offset)
 
     def find_index(self, key):
-        for i, reader in enumerate(self._readers):
-            if key in reader:
-                return self._offsets[i] + reader.find_index(key)
+        for i in range(self._num_shards):
+            with self._reader(i) as reader:
+                if key in reader:
+                    return self._offsets[i] + reader.find_index(key)
         raise KeyError(key)
 
     def get(self, index, yield_key=False, raw=False, copy=True):
         reader_i = bisect_left(self._offsets, index)
         offset = index - self._offsets[reader_i]
-        return self._readers[reader_i].get(offset, yield_key, raw, copy)
+        with self._reader(reader_i) as reader:
+            return reader.get(offset, yield_key, raw, copy)
 
     def slice(self, start, stop=None, yield_key=False, raw=False, copy=True):
         return self.iter(start, stop, yield_key, raw, copy)
@@ -52,18 +67,17 @@ class ShardedReader(Reader):
             chunk_size=16,
     ):
         reader_start = bisect_left(self._offsets, start)
-        reader_stop = bisect_left(self._offsets, stop)
-        for reader, offset in zip(
-                self._readers[reader_start:reader_stop],
-                self._offsets[reader_start:reader_stop],
-        ):
-            for sample in reader.iter(
-                    max(0, start-offset),
-                    stop-offset,
-                    yield_key,
-                    raw,
-                    copy,
-                    chunk_size,
-            ):
-                self._i += 1
-                yield sample
+        reader_stop = bisect_left(self._offsets, stop) + 1
+        for i in range(reader_start, reader_stop):
+            offset = self._offsets[i]
+            with self._reader(i) as reader:
+                for sample in reader.iter(
+                        max(0, start-offset),
+                        stop-offset,
+                        yield_key,
+                        raw,
+                        copy,
+                        chunk_size,
+                ):
+                    self._i += 1
+                    yield sample
