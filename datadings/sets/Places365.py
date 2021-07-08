4a8dfb8896545bdbd5aadf172deda5bbeafeaf0a
Joachim Folz <joachim.folz@dfki.de>
2021-07-08 11:56:01 +0200

    fix Places365 spelling

--- datadings/sets/Places365.py
+++ datadings/sets/Places365.py
@@ -1,5 +1,5 @@
 from .tools import load_json
 
-CLASSES = load_json("Places356_classes.json.xz")
+CLASSES = load_json("Places365_classes.json.xz")
 CLASS_TO_ID = {c: i for i, c in enumerate(CLASSES)}
 ID_TO_CLASS = {i: c for i, c in enumerate(CLASSES)}
