412108cdcc6b793e8055df7b49959f6b41e42832
Joachim Folz <joachim.folz@dfki.de>
2023-04-26 18:21:43 +0200

    consolidate shuffler tests

--- test/test_shuffler.py
+++ test/test_shuffler.py
@@ -7,43 +7,47 @@ from .common import correct_length
 from .common import no_repetitions
 
 
+def make_reader():
+    return Shuffler(MsgpackReader(MSGPACK_PATH))
+
+
 def test_missing_keys():
-    missing_keys(Shuffler(MsgpackReader(MSGPACK_PATH)))
+    missing_keys(make_reader())
 
 
 def test_length():
-    correct_length(Shuffler(MsgpackReader(MSGPACK_PATH)))
+    correct_length(make_reader())
 
 
 def test_length_empty():
-    r = Shuffler(MsgpackReader(MSGPACK_PATH))
+    r = make_reader()
     for i in range(0, len(r), 809):
         correct_length(r, start=i, stop=i)
 
 
 def test_length_start():
-    r = Shuffler(MsgpackReader(MSGPACK_PATH))
+    r = make_reader()
     for i in range(0, len(r) // 2, 809):
         correct_length(r, start=i)
 
 
 def test_length_stop():
-    r = Shuffler(MsgpackReader(MSGPACK_PATH))
+    r = make_reader()
     for i in range(len(r) // 2 + 1, len(r), 809):
         correct_length(r, stop=i)
 
 
 def test_length_stop_negative():
-    r = Shuffler(MsgpackReader(MSGPACK_PATH))
+    r = make_reader()
     for i in range(0, len(r) // 2, 809):
         correct_length(r, stop=-i)
 
 
 def test_length_start_stop():
-    r = Shuffler(MsgpackReader(MSGPACK_PATH))
+    r = make_reader()
     for i in range(0, len(r) // 2, 809):
         correct_length(r, start=i, stop=-i)
 
 
 def test_repetitions():
-    no_repetitions(Shuffler(MsgpackReader(MSGPACK_PATH)))
+    no_repetitions(make_reader())
